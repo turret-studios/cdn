a0600e3a7193b2b0d1880be29d7b635ff19bb8cf
turret-dev <turret@turret-studios.tk>
2020-06-06 23:36:12 -0500

    remove a bunch of useless shit

--- tools/discord.sh
+++
@@ -1,5 +0,0 @@
-curl -L http://canary.discord.com/api/download/canary?platform=linux > /tmp/discord.deb
-sudo echo "sudo"
-sudo apt install libappindicator1 libc++1 libc++1-10 libc++abi1-10 libdbusmenu-gtk4
-sudo dpkg -i /tmp/discord.deb
-rm /tmp/discord.deb
